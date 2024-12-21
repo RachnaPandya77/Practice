8acf9c5ca954012bec84eab41c513dd77b87fb9d
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-12-21 19:47:06 +0530

    dbproject1

---
+++ python/django-practice/DBproject1/DBapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import studinfo
+
+# Register your models here.
+class stud_data(admin.ModelAdmin):
+    ordering = ['id']
+    list_display = ['id','name','city','email','dob','mobile','address']
+
+admin.site.register(studinfo,stud_data)
