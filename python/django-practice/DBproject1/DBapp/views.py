2693edb4ff1f055c4e90d90dcde2ccfaecabfac6
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-12-23 16:49:00 +0530

    add new file

--- python/django-practice/DBproject1/DBapp/views.py
+++ python/django-practice/DBproject1/DBapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .forms import *
 
 # Create your views here.
 def index(request):
+    if request.method=='POST':
+        s=stud(request.POST)
+        if s.is_valid():
+            s.save()
+            print("Data inserted")
+        else:
+            print(s.errors)
     return render(request,'index.html')
