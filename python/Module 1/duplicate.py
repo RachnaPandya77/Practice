69b2f5b25948f077fc819c0fd9f84d107a560cee
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-12-28 10:02:36 +0530

    add file

---
+++ python/Module 1/duplicate.py
@@ -0,0 +1,9 @@
+# remove duplicate
+
+l = 2,3,4,2,5,6,7,4,5,6,7
+x = set(l)
+print("Unique: ",x)
+
+# convert to tuple
+"""x = tuple(l)
+print("tuple",x)"""
---
+++ python/Module 1/mutable.py
@@ -0,0 +1,11 @@
+# mutable
+
+mylist = [1,2,3]
+mylist[0] = 7
+print(mylist)
+
+#immutuable
+
+mytuple = (1,2,3)
+mytuple[0] = 5
+print(mytuple)
