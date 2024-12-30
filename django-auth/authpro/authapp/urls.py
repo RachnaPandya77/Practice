bc44babdfdba964101d70e0fb14482519516fe7a
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-12-30 21:06:55 +0530

    chnages

--- django-auth/authpro/authapp/urls.py
+++ django-auth/authpro/authapp/urls.py
@@ -5,4 +5,6 @@ from authapp import views
 urlpatterns = [
     path('',views.index),
     path('signup/',views.signup),
+    path('home/',views.home,name='home'),
+    path('userlogout/',views.userlogout),
 ]
--- django-auth/authpro/authapp/views.py
+++ django-auth/authpro/authapp/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render,redirect
 from .forms import *
+from django.contrib.auth import logout
 
 # Create your views here.
-def index(request):
-    return render(request,'index.html')
+def index(request):#login
+    msg=""
+    if request.method=='POST':
+        unm=request.POST['username']
+        paas=request.POST['password']
+
+        user=usersignup.objects.filter(username=unm,password=paas)
+        if user:
+            print("Login Successful")
+            msg="Login successfully"
+            request.session['user']=unm #session create
+            return redirect('home')
+        else:
+            print("Login failed")
+            msg="Login failed"
+
+    return render(request,'index.html',{'msg':msg})
 
 def signup(request):
     msg=""
@@ -18,5 +34,14 @@ def signup(request):
             print(newuser.errors)
             msg="Error"
     return render(request,'signup.html',{'msg':msg})
+
+def home(request):
+    user=request.session.get('user')
+    return render(request,'home.html',{'user':user})
+
+def userlogout(request):
+    logout(request)
+    return redirect('/')
+
 
 
